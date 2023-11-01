828be4a3a05aa98b6c46436d6d2321be719dd533
Rednelg <glenderyanez@gmail.com>
2023-11-01 12:33:20 +0100

    feat: Setup project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.astro'],
+  theme: {
+    extend: {
+      colors:{
+        yellow: "#fbffa7",
+        purple: "#b1c5ff",
+        lavender: "#d5b3ff",
+        orange: "#ff764d",
+        blue: "#0000ff",
+        green: "#b6ffc0"
+      }
+    },
+  },
+  plugins: [],
+}
+
